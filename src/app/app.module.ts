1094532189fb3a5997294bc78164ec64c12fd615
phanvietdung2101 <59188700+phanvietdung2101@users.noreply.github.com>
2020-06-06 09:01:56 +0700

    create user-registration component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserRegistrationComponent } from './user-registration/user-registration.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    UserRegistrationComponent
   ],
   imports: [
     BrowserModule,
